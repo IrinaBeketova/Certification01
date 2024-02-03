c10e5a9b5afb73676e2683bba699a0b6bd214ed4
Renard <onlybags111@gmail.com>
2024-02-03 15:52:11 +0300

    Added solution

--- Final_test01/Program.cs
+++ Final_test01/Program.cs
@@ -3,6 +3,31 @@
 // Первоначальный массив можно ввести с клавиатуры, 
 // либо задать на старте выполнения алгоритма. 
 // При решении не рекомендуется пользоваться коллекциями, 
-// лучше обойтись исключительно массивами.
+// лучше обойтись исключительно маcсивом.
+
+string[] array1 = new string[4] {"1234", "1567", "-2", "computer science"};
+string[] array2 = new string[array1.Length];
+void SearchedArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SearchedArray(array1, array2);
+PrintArray(array2);
 
 
